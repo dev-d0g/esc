57b64bf619a5ef12a794d99ec4ee76d136b76efd
dev-d0g <dhart0196@gmail.com>
2025-11-13 11:28:20 -0500

    :D

---
+++ Scripts/silkscreen.py
@@ -0,0 +1,20 @@
+import pcbnew
+
+new_width_mm = 0.75
+new_height_mm = 0.75
+
+board = pcbnew.GetBoard()
+
+for footprint in board.GetFootprints():
+    refdes = footprint.GetReference()
+    if refdes.startswith("U"):
+        continue
+    
+    for text in footprint.GraphicalItems():
+        if isinstance(text, pcbnew.FP_TEXT):
+            size = text.GetTextSize()
+            size.x = pcbnew.FromMM(new_height_mm)
+            size.y = pcbnew.FromMM(new_width_mm)
+            text.SetTextSize(size)
+pcbnew.Refresh()
+print("Silkscreen updated")   
